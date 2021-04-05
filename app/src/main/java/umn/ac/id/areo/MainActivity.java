67e029be0e324ce3a019656241d47c3a8b7167f8
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-05 20:53:59 +0700

    second commit

--- app/src/main/java/umn/ac/id/areo/MainActivity.java
+++ app/src/main/java/umn/ac/id/areo/MainActivity.java
@@ -5,19 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-ImageView logo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mentargertkan komponen pada XML
-        logo = findViewById(R.id.logo);
-
-        //memberi warna pada komponen
-        logo.setBackgroundColor(Color.RED);
     }
 }
