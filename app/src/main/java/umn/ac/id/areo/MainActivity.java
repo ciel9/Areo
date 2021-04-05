7fb340b2777f7f55cd7ced3e2de1e0e8c7f4de75
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-05 22:42:42 +0700

    third commit

--- app/src/main/java/umn/ac/id/areo/MainActivity.java
+++ app/src/main/java/umn/ac/id/areo/MainActivity.java
@@ -2,17 +2,35 @@ package umn.ac.id.areo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //mendapati id pada xml
+        logo = findViewById(R.id.logo);
+
+        //memberikan event kpd logo
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //menuju halaman lain
+                Intent a = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(a);
+                finish();
+            }
+        });
 
     }
 }
