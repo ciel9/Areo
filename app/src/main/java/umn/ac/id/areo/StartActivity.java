b8427e8ce4fcebb7b0e3fcc82259636fc06f058b
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-10 21:16:49 +0700

    commit finish

--- app/src/main/java/umn/ac/id/areo/StartActivity.java
+++ app/src/main/java/umn/ac/id/areo/StartActivity.java
@@ -2,16 +2,30 @@ package umn.ac.id.areo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class StartActivity extends AppCompatActivity {
 
+    Button btn_sign_in;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        btn_sign_in = findViewById(R.id.btn_sign_in);
+
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotosign = new Intent(StartActivity.this, SigninActivity.class);
+                startActivity(gotosign);
+            }
+        });
 
     }
 }
