24dc83fd7e18da674105d023794208b6e526e821
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-11 18:24:07 +0700

    commit finish

--- app/src/main/java/umn/ac/id/areo/SigninActivity.java
+++ app/src/main/java/umn/ac/id/areo/SigninActivity.java
@@ -2,13 +2,27 @@ package umn.ac.id.areo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SigninActivity extends AppCompatActivity {
+
+    TextView btn_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
+
+        btn_register = findViewById(R.id.btn_register);
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent regis = new Intent(SigninActivity.this,RegisterOneActivity.class);
+                startActivity(regis);
+            }
+        });
     }
 }
