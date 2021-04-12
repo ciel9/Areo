5b77649165e1807d9482393c9be997ac8f7afdf8
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-12 16:38:19 +0700

    commit finish

--- app/src/main/java/umn/ac/id/areo/StartActivity.java
+++ app/src/main/java/umn/ac/id/areo/StartActivity.java
@@ -12,18 +12,28 @@ import android.widget.Toast;
 public class StartActivity extends AppCompatActivity {
 
     Button btn_sign_in;
+    Button btn_regis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
         btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_regis = findViewById(R.id.btn_regis);
 
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent gotosign = new Intent(StartActivity.this, SigninActivity.class);
                 startActivity(gotosign);
+            }
+        });
+        btn_regis.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent regis = new Intent(StartActivity.this, RegisterOneActivity.class);
+                startActivity(regis);
             }
         });
 
