13b0b5ffba891bedb5127e833042236ddb0e88e7
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-15 19:25:55 +0700

    commit finish

--- app/src/main/java/umn/ac/id/areo/HomeActivity.java
+++ app/src/main/java/umn/ac/id/areo/HomeActivity.java
@@ -2,13 +2,27 @@ package umn.ac.id.areo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class HomeActivity extends AppCompatActivity {
+
+    LinearLayout btn_ticket_pisa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        btn_ticket_pisa = findViewById(R.id.btn_ticket_pisa);
+        btn_ticket_pisa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent detail = new Intent(HomeActivity.this, TicketDetailActivity.class);
+                startActivity(detail);
+            }
+        });
     }
 }
