0ff7c1cf5c0f48253d2fd5c13c1b6f1bdbbf8729
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-16 14:58:49 +0700

    commit finish

---
+++ app/src/main/java/umn/ac/id/areo/SuccessBuyTicketActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.areo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SuccessBuyTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_buy_ticket);
+    }
+}
--- app/src/main/java/umn/ac/id/areo/TicketCheckoutActivity.java
+++ app/src/main/java/umn/ac/id/areo/TicketCheckoutActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
-public class TicketDetailActivity extends AppCompatActivity {
+public class TicketCheckoutActivity extends AppCompatActivity {
 
     Button btn_buy_ticket;
     LinearLayout btn_back;
@@ -16,22 +16,23 @@ public class TicketDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ticket_detail);
+        setContentView(R.layout.activity_ticket_checkout);
 
         btn_buy_ticket = findViewById(R.id.btn_buy_ticket);
         btn_buy_ticket.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gocheckout = new Intent(TicketDetailActivity.this, TicketCheckoutActivity.class);
-                startActivity(gocheckout);
+                Intent pay = new Intent(TicketCheckoutActivity.this, SuccessBuyTicketActivity.class);
+                startActivity(pay);
             }
         });
+
         btn_back = findViewById(R.id.btn_back);
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent gohome = new Intent(TicketDetailActivity.this, HomeActivity.class);
-                startActivity(gohome);
+                Intent backtodetail = new Intent(TicketCheckoutActivity.this, TicketDetailActivity.class);
+                startActivity(backtodetail);
             }
         });
     }
