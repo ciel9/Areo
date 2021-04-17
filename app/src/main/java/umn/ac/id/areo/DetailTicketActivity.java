8de74e77dd9d1b6b11a75b2f9cf2d405d63bcae6
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-17 21:49:48 +0700

    commit finish

---
+++ app/src/main/java/umn/ac/id/areo/DetailTicketActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.areo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailTicketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_ticket);
+    }
+}
--- app/src/main/java/umn/ac/id/areo/MyProfileActivity.java
+++ app/src/main/java/umn/ac/id/areo/MyProfileActivity.java
@@ -2,13 +2,37 @@ package umn.ac.id.areo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MyProfileActivity extends AppCompatActivity {
+
+    ImageView back2;
+    ImageView ticket_detail1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
+
+        back2 = findViewById(R.id.back2);
+        back2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backtohome = new Intent(MyProfileActivity.this, HomeActivity.class);
+                startActivity(backtohome);
+            }
+        });
+
+        ticket_detail1 = findViewById(R.id.ticket_detail1);
+        ticket_detail1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gototicket = new Intent(MyProfileActivity.this, DetailTicketActivity.class);
+                startActivity(gototicket);
+            }
+        });
     }
 }
