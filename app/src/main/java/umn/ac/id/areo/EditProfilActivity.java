abf972fe6eda8434efcb9625e364903247f8ae32
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-19 16:20:46 +0700

    commit finish

---
+++ app/src/main/java/umn/ac/id/areo/EditProfilActivity.java
@@ -0,0 +1,28 @@
+package umn.ac.id.areo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+public class EditProfilActivity extends AppCompatActivity {
+
+    LinearLayout btn_back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profil);
+
+        btn_back = findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent backprofile = new Intent(EditProfilActivity.this, MyProfileActivity.class);
+                startActivity(backprofile);
+            }
+        });
+    }
+}
--- app/src/main/java/umn/ac/id/areo/MyProfileActivity.java
+++ app/src/main/java/umn/ac/id/areo/MyProfileActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MyProfileActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MyProfileActivity extends AppCompatActivity {
     ImageView back2;
     ImageView ticket_detail1;
     ImageView image2;
+    Button btn_edit_profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +43,15 @@ public class MyProfileActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent gototicket2 = new Intent(MyProfileActivity.this, DetailTicketActivity.class);
                 startActivity(gototicket2);
+            }
+        });
+
+        btn_edit_profile = findViewById(R.id.btn_edit_profile);
+        btn_edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent editprofil = new Intent(MyProfileActivity.this, EditProfilActivity.class);
+                startActivity(editprofil);
             }
         });
     }
