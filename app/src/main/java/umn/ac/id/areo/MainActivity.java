070650cec2f8c0a2fea888300684bf7594f955fb
Yohana Cindy Elsanjaya <68240501+ciel9@users.noreply.github.com>
2021-04-24 18:25:06 +0700

    commit finish

--- app/src/main/java/umn/ac/id/areo/MainActivity.java
+++ app/src/main/java/umn/ac/id/areo/MainActivity.java
@@ -7,12 +7,16 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView logo;
+    Animation app_splash, btt;
+    TextView app_logo;
+    TextView app_subtitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //mendapati id pada xml
-        logo = findViewById(R.id.logo);
+        app_logo = findViewById(R.id.app_logo);
+        app_subtitle = findViewById(R.id.app_subtitle);
+
+        //load animation
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
+        //run animation
+        app_logo.startAnimation(app_splash);
+        app_subtitle.startAnimation(btt);
 
         //membuat timer untuk pindah activity secara otomatis
         Handler handler = new Handler();
